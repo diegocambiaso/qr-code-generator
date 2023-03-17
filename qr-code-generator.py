ac249dd9e5d5203493dce8498d5a0901d596aa9a
Diego Cambiaso <diegocambiaso@gmail.com>
2023-03-17 12:41:33 -0300

    Update qr-code-generator.py

--- qr-code-generator.py
+++ qr-code-generator.py
@@ -1,4 +1,14 @@
-mport pyqrcode
+#!/usr/bin/env python
+# coding=utf8
+#
+# Copyright (C) 2023, Diego Cambiaso
+# GNU General Public License v3.0
+
+'''
+This script is for educational purposes and will let you generate a QR-Code from an URL.
+'''
+
+import pyqrcode
 
 __version__ = 1
 
